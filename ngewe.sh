57b7e1e9e3e7c4da15b08476dc32d7537cdf9818
Faizulboften <72049391+Faizulboften@users.noreply.github.com>
2020-10-01 14:24:26 +0800

    Create ngewe.sh

---
+++ ngewe.sh
@@ -0,0 +1,73 @@
+bi='\033[34;1m' #biru
+ij='\033[32;1m' #ijo
+pr='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+or='\033[1;38;5;208m' #Orange
+echo $purple "
+●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬●
+
+✔ FB : FAIZUL 📂
+✔ Wa : 082271426251 📂
+✔ SCRIP HACKING BY FAIZUL 📂
+✔ YT : HTC CTR GAMING 📂
+
+●▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬● " 
+echo $cy "
+━━━━━┫{⭕ MENU HACK UPDATE ⭕}┣━━━━━
+ ▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬● 
+[1] : 🔘 REPORT AKUN FACEBOOK 🔘
+[2] : 🔘 HACK TARGET CRACKED  🔘
+[3] : 🔘 SQL MAP              🔘
+ ▬▬▬▬▬▬▬▬▬๑۩۩๑▬▬▬▬▬▬▬▬● 
+ ┠
+ ┗────[4] exit ✘"
+ echo '\033[35;1m'
+ read -p "root@Pilih Nomor > " bro
+ 
+ if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+echo $cyan " MENGGINSTALL DULU"
+sleep 30
+pkg install git
+pip2 install mechanize
+git clone https://github.com/ILayTamvan/report.git
+cd Report
+unzip Report. py
+python2 Report. p
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+echo $cyan " MENGINSTALL DULU"
+sleel 30
+pkg install git
+pip2 install mechanize
+git clone https://github.com/Kandol/CrackFacebook
+cd CrackFacebook
+sh Crackfacebook.sh
+fi
+
+if [ $bro = 3 ] || [ $bro = 2 ]
+then
+clear
+echo $cyan " MENGGINSTALL DULU"
+sleep 30
+pkg install git
+git clone https://github.com/sqlmapproject/sqlmap
+cd sqlmap
+python2 sqlmap.py
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+echo $cyan  "SUBSCRIBER YT HTC CTR GAMIG "
+sleep 50
+exit
+fi
+
+ 
